276174304336ecdf195d2518f1fb10a5355895d0
Bhavuk-Devex <bhavukbansal@devexhub.in>
2025-04-15 18:00:11 +0530

    update server filer

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-// server.js
 const express = require("express");
-const sslRedirect = require("heroku-ssl-redirect");
 const path = require("path");
-
 const app = express();
 
-// Enable SSL redirect
-app.use(sslRedirect());
-
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
